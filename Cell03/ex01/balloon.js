631ea71427a593fa1d07bd700d56035f803b9ed3
ICEMONSTER1 <plohamas1@gmail.com>
2026-09-15 21:51:57 +0700

    complete Cell03 ex01

---
+++ Cell03/ex01/balloon.js
@@ -0,0 +1,36 @@
+const balloon = document.getElementById("balloon");
+
+let size = 200;
+let colorIndex = 0;
+
+const colors = ["red", "green", "blue"];
+
+balloon.addEventListener("click", function () {
+    size += 10;
+
+    if (size > 420) {
+        size = 200;
+    }
+
+    colorIndex = (colorIndex + 1) % colors.length;
+
+    balloon.style.width = size + "px";
+    balloon.style.height = size + "px";
+    balloon.style.backgroundColor = colors[colorIndex];
+});
+
+balloon.addEventListener("mouseleave", function () {
+    if (size > 200) {
+        size -= 5;
+
+        if (size < 200) {
+            size = 200;
+        }
+    }
+
+    colorIndex = (colorIndex - 1 + colors.length) % colors.length;
+
+    balloon.style.width = size + "px";
+    balloon.style.height = size + "px";
+    balloon.style.backgroundColor = colors[colorIndex];
+});
